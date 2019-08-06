dd517c1d06d79c9c851ca0debeee8e64bce11a87
miltonmurray <miltonmurray@github.com>
2019-08-05 22:08:28 -0400

    Added Textile css grid

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,13 @@
+import './ImageList.css';
 import React from 'react'
+import ImageCard from './ImageCard'
 
 const ImageList = (props)=>{
     const images = props.images.map((image) =>{    
-        return <img key={image.id} src={image.urls.regular} alt={image.alt_description} />
+        return <ImageCard key={image.id} image={image}/>
     });
-    console.log(props);
-    return <div>{images}</div>;
+    
+    return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
